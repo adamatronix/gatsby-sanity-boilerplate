7ca13aa17a82eca71743300b1e95d6efd405f9d2
adamatronix <you@example.com>
2022-07-12 10:35:25 +0800

    setup Add global style boilerplate

--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./header";
+import { GlobalStyle } from 'utils/global';
 
 import "../styles/layout.css";
 import * as styles from "./layout.module.css";
 
 const Layout = ({ children, onHideNav, onShowNav, showNav, siteTitle }) => (
   <>
+    <GlobalStyle />
     <Header siteTitle={siteTitle} onHideNav={onHideNav} onShowNav={onShowNav} showNav={showNav} />
     <div className={styles.content}>{children}</div>
     <footer className={styles.footer}>
