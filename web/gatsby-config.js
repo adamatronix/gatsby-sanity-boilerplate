5c9f676f7f1160850f9da4221b217f58341879b4
adamatronix <you@example.com>
2022-07-12 10:19:37 +0800

    setup Add styled components

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-postcss',
     'gatsby-plugin-image',
+    'gatsby-plugin-styled-components',
     'gatsby-plugin-react-helmet',
     "gatsby-plugin-resolve-src",
     {
