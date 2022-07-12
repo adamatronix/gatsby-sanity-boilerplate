6f934724ce54cef5e280194ecc05f636031ae4b5
adamatronix <you@example.com>
2022-07-12 10:30:27 +0800

    setup Add site context and include sanity client

--- web/gatsby-ssr.js
+++ web/gatsby-ssr.js
@@ -5,3 +5,9 @@
  */
 
 // You can delete this file if you're not using it
+import * as React from "react";
+import { SiteProvider } from "./src/context/site-context";
+
+export const wrapRootElement = ({ element }) => (
+  <SiteProvider>{element}</SiteProvider>
+);
